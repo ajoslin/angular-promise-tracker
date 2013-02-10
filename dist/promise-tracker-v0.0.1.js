5fac0d8ff8cb94989172e5338cb219d6006fe1c9
Andy Joslin <andytjoslin@gmail.com>
2013-02-10 00:51:39 -0500

    v0.1.0

---
+++ dist/promise-tracker-v0.0.1.js
@@ -0,0 +1,105 @@
+/*
+ * angular-promise-tracker, version 0.1.0
+ * http://github.com/ajoslin/angular-promise-tracker
+ * Licensed with MIT License
+ */
+
+angular.module('promiseTracker', [])
+.factory('promiseTracker', function($q) {
+  var self = this;
+  var trackers = {};
+
+  function Tracker(name) {
+    var self = this;
+    var numPending = 0;
+    var callbacks = {
+      start: [], //called when a new promise is added
+      done: [], //called when a promise is resolved (error or success)
+      error: [], //called on error
+      success: [] //called on success
+    };
+    self.active = function() {
+      return numPending > 0;
+    };
+    /*
+     * on: add a callback for an event
+     * @param event: 'start', 'done', 'error', or 'success'
+     * @param callback: function() {} to call when event happens
+     * @return function removeCallback: removes listener
+     */
+    self.on = self.bind = function(event, cb) {
+      if (!callbacks[event]) {
+        throw "Cannot create callback for event '" + event + 
+        "'. Allowed types: 'start', 'done', 'error', 'success'";
+      }
+      callbacks[event].push(cb);
+      return self;
+    };
+    self.off = self.unbind = function(event, cb) {
+      if (!callbacks[event]) {
+        throw "Cannot create callback for event '" + event + 
+        "'. Allowed types: 'start', 'done', 'error', 'success'";
+      }
+      if (cb) {
+        var idx = callbacks[event].indexOf(cb);
+        callbacks[event].splice(cb, 1);
+      } else {
+        //Erase all events of this type if no cb specified to remvoe
+        callbacks[event].length = 0;
+      }
+    };
+
+    function promiseDone(value, isError) {
+      fireEvent('done', [value, isError]);
+      if (isError) fireEvent('error', [value]);
+      else fireEvent('success', [value]);
+      numPending--;
+    }
+    function fireEvent(event, params) {
+      angular.forEach(callbacks[event], function(cb) {
+        cb.apply(self, params || []);
+      });
+    }
+    /* Adds any old promise to our tracking */
+    /* startParam is usually an htttp request config object */
+    self.addPromise = function(promise, startParam) {
+      numPending++;
+      fireEvent('start', [startParam]);
+      
+      return promise.then(function success(value) {
+        promiseDone(value);
+        return value;
+      }, function error(value) {
+        promiseDone(value, true);
+        return $q.reject(value);
+      });
+    };
+  }
+  return function(trackerName) {
+    if (!trackers[trackerName]) trackers[trackerName] = new Tracker(trackerName);
+    return trackers[trackerName];
+  };
+})
+.config(function($httpProvider) {
+  $httpProvider.responseInterceptors.push('trackerResponseInterceptor');
+})
+.factory('trackerResponseInterceptor', function($q, promiseTracker, $injector) {
+  //We use $injector get around circular dependency problem for $http
+  var $http;
+  return function spinnerResponseInterceptor(promise) {
+    if (!$http) $http = $injector.get('$http'); 
+    
+    //We know the latest request is always going to be last in the list
+    var requestConfig = $http.pendingRequests[$http.pendingRequests.length-1];
+    var trackerConfig;
+    if ((trackerConfig = requestConfig.tracker)) {
+      if (!angular.isArray(trackerConfig)) {
+        trackerConfig = [trackerConfig];
+      }
+      angular.forEach(trackerConfig, function(trackerName) {
+        promiseTracker(trackerName).addPromise(promise, requestConfig);
+      });
+    }
+    return promise;
+  };
+});
