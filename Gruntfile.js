d186b3c642fe4d6594b31ca9e530b96af2f0251d
Pascal Precht <pascal.precht@googlemail.com>
2013-12-03 11:34:44 +0100

    chore(grunt): adds load-grunt-tasks module
    this commit adds the load-grunt-tasks module which makes it easier to
    maintain grunt tasks that have to be loaded.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,6 @@
 module.exports = function (grunt) {
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-conventional-changelog');
-  grunt.loadNpmTasks('grunt-karma');
+  require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
     dist: 'dist',
